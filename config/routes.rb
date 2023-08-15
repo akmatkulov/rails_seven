ed7651541fa5ce0eb7ef6c5aa032d80bc35c4af7
akmatkulov <akmatkulovs991@gmail.com>
2023-08-16 00:16:30 +0600

    Add show comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
-  resources :articles
+  
+  #вложенный маршрут
+  resources :articles do
+    resources :comments
+  end
 end
